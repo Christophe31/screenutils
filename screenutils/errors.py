fa5b47883e4c063f6514d203c1ab58a08723e382
Daniel Harris <dh.harris183@gmail.com>
2016-06-01 13:46:35 -0700

    Quotes around name in error message

--- screenutils/errors.py
+++ screenutils/errors.py
@@ -3,6 +3,6 @@
 class ScreenNotFoundError(Exception):
     """raised when the screen does not exists"""
     def __init__(self, message, screen_name):
-        message += " Screen {0} not found".format(screen_name)
+        message += " Screen \"{0}\" not found".format(screen_name)
         self.screen_name = screen_name
         super(ScreenNotFoundError, self).__init__(message)

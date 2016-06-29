4180fd58ceac2a9aa76d798881bf512d6532aa65
Christophe Narbonne <Christophe31@gmail.com>
2016-06-29 12:51:02 +0200

    setup.py: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='screenutils',
-    version='0.0.1.5.5',
+    version='0.0.1.6.0',
     packages=['screenutils',],
     license='GNU Public License >=2 (ask me if you want other)',
     author="Christophe Narbonne",

dafc41f5fdd25c9bf2102e7b0cb1e3658b239a10
Narbonne <Christophe31@users.noreply.github.com>
2017-02-17 22:59:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='screenutils',
-    version='0.0.1.6.1',
+    version='0.0.1.6.2',
     packages=['screenutils',],
     license='GNU Public License >=2 (ask me if you want other)',
     author="Christophe Narbonne",
